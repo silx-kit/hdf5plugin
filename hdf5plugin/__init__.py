b72cbe33167a23a63651bc076ee4f4321e43f5c2
V. Armando Sole <sole@esrf.fr>
2016-08-17 17:58:06 +0200

    Update date.

--- hdf5plugin/__init__.py
+++ hdf5plugin/__init__.py
@@ -27,7 +27,7 @@ under windows."""
 
 __authors__ = ["V.A. Sole"]
 __license__ = "MIT"
-__date__ = "06/07/2016"
+__date__ = "17/08/2016"
 
 import os
 import sys
@@ -36,8 +36,6 @@ if sys.platform.startswith("darwin"):
     compiler = "darwin"
     arch = None
 elif sys.platform.startswith("win"):
-    if ("h5py" in sys.modules) or ("PyTables" in sys.modules):
-        raise ImportError("You should import hdf5plugin before importing h5py or PyTables")
     if sys.version.startswith("3.5"):
         compiler = "VS2015"
     elif sys.version.startswith("2.7"):
@@ -54,6 +52,8 @@ else:
    compiler = None
 
 if compiler is not None:
+    if ("h5py" in sys.modules) or ("PyTables" in sys.modules):
+        raise ImportError("You should import hdf5plugin before importing h5py or PyTables")
     current_path = os.getenv("HDF5_PLUGIN_PATH", None)
     plugin_path = os.path.join(os.path.dirname(__file__),
                                        compiler)
