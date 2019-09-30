f9d282875a497ae5e3fbae4dd6a2700d7e23803f
Thomas VINCENT <thomas.vincent@esrf.fr>
2019-09-30 14:29:19 +0200

    Add HDF5 plugin with dynamic loading

--- hdf5plugin/__init__.py
+++ hdf5plugin/__init__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # /*##########################################################################
 #
-# Copyright (c) 2016-2018 European Synchrotron Radiation Facility
+# Copyright (c) 2016-2019 European Synchrotron Radiation Facility
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,28 +25,24 @@
 """This module provides compiled shared libraries for their use as HDF5 plugins
 under windows, MacOS and linux."""
 
-__authors__ = ["V.A. Sole", "H. Payno"]
+__authors__ = ["V.A. Sole", "H. Payno", "T. Vincent"]
 __license__ = "MIT"
-__date__ = "11/07/2018"
+__date__ = "30/09/2019"
 
+import ctypes
+from glob import glob as _glob
+import logging
 import os
 import sys
+
+
+_logger = logging.getLogger(__name__)
 
 
 if sys.platform.startswith("darwin"):
     compiler = "darwin"
     arch = None
     path_separator = ":"
-elif sys.platform.startswith("linux"):
-    if sys.maxsize > 2**32:
-        # 64 bit
-        arch = "x86_64"
-        compiler = "manylinux"
-    else:
-        # 32 bit
-        arch = None
-        compiler = None
-    path_separator = ":"    
 elif sys.platform.startswith("win"):
     if sys.version_info >= (3, 5):
         compiler = "VS2015"
@@ -62,7 +58,7 @@ elif sys.platform.startswith("win"):
         arch = "x86"
     path_separator = ";"
 else:
-   compiler = None
+    compiler = None
 
 if compiler is not None:
     if ("h5py" in sys.modules) or ("PyTables" in sys.modules):
@@ -78,3 +74,57 @@ if compiler is not None:
         os.environ["HDF5_PLUGIN_PATH"]  = plugin_path
     else:
         os.environ["HDF5_PLUGIN_PATH"] = current_path + path_separator + plugin_path
+
+
+PLUGINS_PATH = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), 'plugins'))
+"""Path where HDF5 filter plugins are stored"""
+
+
+# IDs of provided filters
+BLOSC = 32001
+"""Blosc filter ID"""
+
+BSHUF = 32008
+"""Bitshuffle filter ID"""
+
+LZ4 = 32004
+"""LZ4 filter ID"""
+
+
+def _init_plugins():
+    """Initialise and register HDF5 filter plugins with h5py
+
+    Generator of tuples: (filename, library handle)
+    """
+    import h5py
+
+    for filename in _glob(os.path.join(PLUGINS_PATH, 'libh5*')):
+        # Load DLL
+        lib = ctypes.CDLL(filename)
+
+        if hasattr(lib, 'init_plugin'):
+            # There is a init_plugin function: initialize DLL
+            lib.init_plugin.restype = None
+            lib.init_plugin(bytes(h5py.h5z.__file__, encoding='utf-8'))
+
+        # Register plugin with h5py's libhdf5
+        # TODO check plugin_type = lib.H5PLget_plugin_type()
+
+        # Get plugin info struct
+        lib.H5PLget_plugin_info.restype = ctypes.c_void_p
+        plugin_info = lib.H5PLget_plugin_info()
+
+        # Register plugin to h5py
+        # TODO h5py.h5z.register(plugin_info)
+        h5zlib = ctypes.CDLL(h5py.h5z.__file__)
+        h5zlib.H5Zregister.argtypes = [ctypes.c_void_p]
+        h5zlib.H5Zregister.restype = ctypes.c_int
+        retval = h5zlib.H5Zregister(plugin_info)
+        if retval != 0:
+            _logger.error('cannot register plugin: %s', filename)
+        else:
+            yield filename, lib
+
+
+_plugins = dict(_init_plugins())  # Store loaded plugins
