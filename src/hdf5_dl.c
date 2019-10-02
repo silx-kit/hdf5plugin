5d1e0240e3b14a6a257fa7da5a41dc2666626551
Thomas VINCENT <thomas.vincent@esrf.fr>
2019-10-02 12:10:17 +0200

    remove windows support (not needed)

--- src/hdf5_dl.c
+++ src/hdf5_dl.c
@@ -30,21 +30,9 @@
  * HDF5 library.
  */
 #include <stdarg.h>
+#include <dlfcn.h>
 #include "hdf5.h"
 #include "H5PLextern.h"
-
-#ifdef _WIN32
-#include <windows.h>
-#define DL_OPEN(libname) LoadLibraryExW(libname, NULL, LOAD_WITH_ALTERED_SEARCH_PATH)
-#define DL_SYM(handle, symbol) GetProcAddress(handle, symbol)
-
-#else
-#include <dlfcn.h>
-#define DL_OPEN(libname) dlopen(libname, RTLD_LAZY | RTLD_LOCAL)
-#define DL_SYM(handle, symbol) dlsym(handle, symbol)
-
-#endif /*_WIN32*/
-
 
 
 /*Function types*/
@@ -110,51 +98,41 @@ hid_t H5E_ERR_CLS_g = -1;
 static bool is_init = false;
 
 
-/* Handle difference between dlopen and LoadLibraryExW */
-#ifdef _WIN32
-typedef const wchar_t* libname_t; /* Using utf-16-le encoding */
-#else
-typedef const char* libname_t;
-#endif /*_WIN32*/
-
-
 /* Initialize the dynamic loading of symbols and register the plugin
  *
  * libname: Name of the DLL from which to load libHDF5 symbols
  * Returns: a value < 0 if an error occured
  */
-int init_filter(libname_t libname)
+int init_filter(const char* libname)
 {
     int retval = -1;
   	void * handle;
 
-    handle = DL_OPEN(libname);
+    handle = dlopen(libname);
 
     if (handle != NULL) {
         /*H5*/
-        DL_H5Functions.H5open = (DL_func_H5open)DL_SYM(handle, "H5open");
+        DL_H5Functions.H5open = (DL_func_H5open)dlsym(handle, "H5open");
         /*H5E*/
-        DL_H5Functions.H5Epush1 = (DL_func_H5Epush1)DL_SYM(handle, "H5Epush1");
-        DL_H5Functions.H5Epush2 = (DL_func_H5Epush2)DL_SYM(handle, "H5Epush2");
+        DL_H5Functions.H5Epush1 = (DL_func_H5Epush1)dlsym(handle, "H5Epush1");
+        DL_H5Functions.H5Epush2 = (DL_func_H5Epush2)dlsym(handle, "H5Epush2");
         /*H5P*/
-        DL_H5Functions.H5Pget_filter_by_id2 = (DL_func_H5Pget_filter_by_id2)DL_SYM(handle, "H5Pget_filter_by_id2");
-        DL_H5Functions.H5Pget_chunk = (DL_func_H5Pget_chunk)DL_SYM(handle, "H5Pget_chunk");
-        DL_H5Functions.H5Pmodify_filter = (DL_func_H5Pmodify_filter)DL_SYM(handle, "H5Pmodify_filter");
+        DL_H5Functions.H5Pget_filter_by_id2 = (DL_func_H5Pget_filter_by_id2)dlsym(handle, "H5Pget_filter_by_id2");
+        DL_H5Functions.H5Pget_chunk = (DL_func_H5Pget_chunk)dlsym(handle, "H5Pget_chunk");
+        DL_H5Functions.H5Pmodify_filter = (DL_func_H5Pmodify_filter)dlsym(handle, "H5Pmodify_filter");
         /*H5T*/
-        DL_H5Functions.H5Tget_size = (DL_func_H5Tget_size)DL_SYM(handle, "H5Tget_size");
-        DL_H5Functions.H5Tget_class = (DL_func_H5Tget_class)DL_SYM(handle, "H5Tget_class");
-        DL_H5Functions.H5Tget_super = (DL_func_H5Tget_super)DL_SYM(handle, "H5Tget_super");
-        DL_H5Functions.H5Tclose = (DL_func_H5Tclose)DL_SYM(handle, "H5Tclose");
+        DL_H5Functions.H5Tget_size = (DL_func_H5Tget_size)dlsym(handle, "H5Tget_size");
+        DL_H5Functions.H5Tget_class = (DL_func_H5Tget_class)dlsym(handle, "H5Tget_class");
+        DL_H5Functions.H5Tget_super = (DL_func_H5Tget_super)dlsym(handle, "H5Tget_super");
+        DL_H5Functions.H5Tclose = (DL_func_H5Tclose)dlsym(handle, "H5Tclose");
         /*H5Z*/
-        DL_H5Functions.H5Zregister = (DL_func_H5Zregister)DL_SYM(handle, "H5Zregister");
+        DL_H5Functions.H5Zregister = (DL_func_H5Zregister)dlsym(handle, "H5Zregister");
 
         /*Variables*/
-#ifndef _WIN32 /* This crashes on Windows... */
-        H5E_CANTREGISTER_g = *((hid_t *)DL_SYM(handle, "H5E_CANTREGISTER_g"));
-        H5E_CALLBACK_g = *((hid_t *)DL_SYM(handle, "H5E_CALLBACK_g"));
-        H5E_PLINE_g = *((hid_t *)DL_SYM(handle, "H5E_PLINE_g"));
-        H5E_ERR_CLS_g = *((hid_t *)DL_SYM(handle, "H5E_ERR_CLS_g"));
-#endif /*_WIN32*/
+        H5E_CANTREGISTER_g = *((hid_t *)dlsym(handle, "H5E_CANTREGISTER_g"));
+        H5E_CALLBACK_g = *((hid_t *)dlsym(handle, "H5E_CALLBACK_g"));
+        H5E_PLINE_g = *((hid_t *)dlsym(handle, "H5E_PLINE_g"));
+        H5E_ERR_CLS_g = *((hid_t *)dlsym(handle, "H5E_ERR_CLS_g"));
 
         /*Register plugin*/
         retval = H5Zregister(H5PLget_plugin_info());
@@ -255,4 +233,3 @@ herr_t H5Zregister(const void *cls)
 {
 CALL(-1, H5Zregister, cls)
 }
-
