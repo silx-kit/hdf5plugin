64b7758b85c7b57a1be5505a892f34ed0d3b815e
Thomas VINCENT <thomas.vincent@esrf.fr>
2019-10-03 11:50:00 +0200

    add virtualenv to path

--- ci/travis_osx.sh
+++ ci/travis_osx.sh
@@ -34,6 +34,7 @@ else
     function travis_osx_install_begin {
         echo Mac OS X install begin: Install pip and setup build venv
         set -x  # echo on
+        export PATH=${PATH}:/Users/travis/Library/Python/2.7/bin
         curl -O https://bootstrap.pypa.io/get-pip.py
         python get-pip.py --user
 
