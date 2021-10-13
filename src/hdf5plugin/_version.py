defd385cf0272ee705735f91eafc6a35bc579a5e
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-10-13 15:02:20 +0200

    bump to 3.2.0

--- src/hdf5plugin/_version.py
+++ src/hdf5plugin/_version.py
@@ -70,8 +70,8 @@ PRERELEASE_NORMALIZED_NAME = {"dev": "a",
                               "candidate": "rc"}
 
 MAJOR = 3
-MINOR = 1
-MICRO = 1
+MINOR = 2
+MICRO = 0
 RELEV = "final"  # <16
 SERIAL = 0  # <16
 
