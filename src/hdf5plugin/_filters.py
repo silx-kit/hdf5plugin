c929af435a12d561714ab4fe429e9bbb14c974b8
Thomas VINCENT <thomas.vincent@esrf.fr>
2022-11-24 17:14:22 +0100

    Update SZ docstring

--- src/hdf5plugin/_filters.py
+++ src/hdf5plugin/_filters.py
@@ -416,7 +416,7 @@ class Zfp(_FilterRefClass):
 class SZ(_FilterRefClass):
     """``h5py.Group.create_dataset``'s compression arguments for using SZ filter.
 
-    For more details about the compressor `SZ <https://https://szcompressor.org/>`_.
+    For more details about the compressor `SZ <https://szcompressor.org/>`_.
     It can be passed as keyword arguments:
 
     .. code-block:: python
@@ -431,7 +431,7 @@ class SZ(_FilterRefClass):
     This filter provides different modes:
 
     - **Absolute** mode: To use, set the ``absolute`` argument.
-      It ensures that the resulting values will be within the absolute tolerance provided with the argument.
+      It ensures that the resulting values will be within the provided absolute tolerance.
 
       .. code-block:: python
 
@@ -441,8 +441,8 @@ class SZ(_FilterRefClass):
               **hdf5plugin.Zfp(absolute=0.1))
 
     - **Relative** mode: To use, set the ``relative`` argument.
-      It ensures that the resulting values will be within the relative tolerance provided with the argument.
-      The tolerance will be computed by multiplying the the argument provided by the range of the data values.
+      It ensures that the resulting values will be within the provided relative tolerance.
+      The tolerance will be computed by multiplying the provided argument by the range of the data values.
 
       .. code-block:: python
 
@@ -452,8 +452,7 @@ class SZ(_FilterRefClass):
               **hdf5plugin.SZ(relative=0.01))
 
     - **Point-wise relative** mode: To use, set the ``pointwise_relative`` argument.
-      It ensures that each grid point of the resulting values will be within the relative tolerance provided with the
-      argument.
+      It ensures that each grid point of the resulting values will be within the provided relative tolerance.
 
       .. code-block:: python
 
