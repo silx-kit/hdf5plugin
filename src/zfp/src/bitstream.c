fd53bbe6219c16557aff80ac38214940594acd1f
Thomas VINCENT <thomas.vincent@esrf.fr>
2025-09-16 13:54:34 +0200

    Squashed 'lib/snappy/' changes from 2c94e1114..6af9287fb
    6af9287fb Release 1.2.2
    de19405e5 Merge pull request #203 from haney/msvc_shared
    4dd78f2de Export symbols from snappy_test_support when building shared
    b395cf7a4 Merge pull request #197 from rp42/main
    fa07e8a94 Fix data corruption when output of snappy compression is more than 4GB
    49087d4e1 Merge pull request #200 from gruenich/feature/cmake-3.10
    a688be4b7 [cmake] Require CMake version 3.10 or newer
    a6b9f87f8 Support clang-cl on Windows
    32ded457c Update CMakeLists NEON flag to reflect only AArch64 NEON optimizations
    fcf1982ac Update bazel module
    58d911b28 Add missing functional include for std::less_equal
    b49982cc1 Merge pull request #179 from mmorel-35/bzlmod
    07406b938 Merge pull request #181 from AtomicRobotMan0101/main
    dc3577f5b Update README.md
    09d30d36f chore(bazel): add MODULE.bazel files for bzlmod
    
    git-subtree-dir: lib/snappy
    git-subtree-split: 6af9287fbdb913f0794d0148c6aa43b58e63c8e3

--- src/zfp/src/bitstream.c
+++
@@ -1,4 +0,0 @@
-#include "zfp/bitstream.h"
-#include "zfp/bitstream.inl"
-
-const size_t stream_word_bits = wsize;
--- src/zfp/tests/constants/floatConsts.h
+++
@@ -1,3 +0,0 @@
-#define FL_PT_DATA
-#define SCALAR_BITS 32
-#define ZFP_TYPE zfp_type_float
